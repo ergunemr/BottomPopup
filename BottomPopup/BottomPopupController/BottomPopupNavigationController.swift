88229491004d82232d4faecff1a876f6f8f84f8f
Emre <ergunemr@itu.edu.tr>
2018-10-11 16:36:38 +0300

    Add necessary initialization methods to bottom popup navigation controller.

--- BottomPopup/BottomPopupController/BottomPopupNavigationController.swift
+++ BottomPopup/BottomPopupController/BottomPopupNavigationController.swift
@@ -1,34 +1,14 @@
 //
-//  BottomPopupViewController.swift
-//  Trendyol
+//  BottomPopupNavigationController.swift
+//  BottomPopup
 //
-//  Created by Emre on 11.09.2018.
-//  Copyright © 2018 Trendyol.com. All rights reserved.
+//  Created by Emre on 11.10.2018.
+//  Copyright © 2018 Emre. All rights reserved.
 //
 
 import UIKit
 
-typealias BottomPresentableViewController = BottomPopupAttributesDelegate & UIViewController
-
-public protocol BottomPopupAttributesDelegate: class {
-    func getPopupHeight() -> CGFloat
-    func getPopupTopCornerRadius() -> CGFloat
-    func getPopupPresentDuration() -> Double
-    func getPopupDismissDuration() -> Double
-    func shouldPopupDismissInteractivelty() -> Bool
-    func getDimmingViewAlpha() -> CGFloat
-}
-
-public struct BottomPopupConstants {
-    static let kDefaultHeight = CGFloat(377.0)
-    static let kDefaultTopCornerRadius = CGFloat(10.0)
-    static let kDefaultPresentDuration = 0.5
-    static let kDefaultDismissDuration = 0.5
-    static let dismissInteractively = true
-    static let kDimmingViewDefaultAlphaValue: CGFloat = 0.5
-}
-
-open class BottomPopupViewController: UIViewController, BottomPopupAttributesDelegate {
+open class BottomPopupNavigationController: UINavigationController, BottomPopupAttributesDelegate {
     
     private var transitionHandler: BottomPopupTransitionHandler?
     
--- BottomPopup/BottomPopupController/BottomPopupTransitionHandler.swift
+++ BottomPopup/BottomPopupController/BottomPopupTransitionHandler.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-typealias BottomPresentableViewController = BottomPopupAttributesDelegate & UIViewController
-
 class BottomPopupTransitionHandler: NSObject, UIViewControllerTransitioningDelegate {
     
     private var presentAnimator: BottomPopupPresentAnimator!
