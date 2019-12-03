939420585ef9c1fe193358d82764acc98ed3b666
Emre <ergunemr@gmail.com>
2019-12-03 09:34:49 +0300

    Create package file for spm.

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "BottomPopup",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10),
+    ],
+    products: [
+        .library(name: "BottomPopup", targets: ["BottomPopup"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .target(name: "BottomPopup", path: "BottomPopup/BottomPopupController/"),
+    ]
+)
